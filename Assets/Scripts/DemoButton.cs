328e8d55d1536de52ed8c16962e1b5c934b8a0e9
Djuensie <djuensiecherfils.sia@gmail.com>
2024-01-06 16:07:59 -0500

    Working on Computer Ui - Added a button to move to a new scene

---
+++ Assets/Scripts/DemoButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoBu : MonoBehaviour
+{
+
+    int counter;
+
+    // Start is called before the first frame update
+    public void ButtonPressed()
+    {
+        counter++;
+        Debug.Log("Game Start");
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
