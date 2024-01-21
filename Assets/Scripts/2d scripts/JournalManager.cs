69f4e0b34ab2e1f9d907e9b71407bbda997cbf45
Aidan Ainsley <aidanainsley@gmail.com>
2024-01-21 15:09:50 -0500

    Journal and Note System essentially done!
    +other fixes +funny +getting npcs to detect and take away items.

--- Assets/Scripts/2d scripts/JournalManager.cs
+++ Assets/Scripts/2d scripts/JournalManager.cs
@@ -6,11 +6,15 @@ using TMPro;
 public class JournalManager : MonoBehaviour
 {
     public Vector2 selectorPos;
-    public Vector2 prevSelect;
-    public TMP_Text[,] textArray = new TMP_Text[5,5];
-    public bool[] hasNote = new bool[25];
+    public Vector2 prevSelectorPos;
+    public Vector2 selection;
+    public Vector2 prevSelection;
+    public TMP_Text[,] textArray = new TMP_Text[5, 5];
+    public List<NoteScript> notes;
     public GameObject TextArray;
     public TMP_Text def;
+    public TMP_Text noteTitle;
+    public TMP_Text noteLore;
     public PlayerScript2D playerScript;
     public Canvas journal;
 
@@ -20,13 +24,16 @@ public class JournalManager : MonoBehaviour
         journal.GetComponent<Canvas>().enabled = false;
 
         selectorPos = Vector2.zero;
-        for (int i = 0; i < textArray.GetLength(0); i++)
+        selection = Vector2.zero;
+        prevSelectorPos = Vector2.up;
+        prevSelection = Vector2.up;
+        for (int i = 0; i < 5; i++)
         {
-            for (int j = 0; j < textArray.GetLength(1); j++)
+            for (int j = 0; j < 5; j++)
             {
                 TMP_Text text = Instantiate(def, TextArray.transform);
-                text.rectTransform.localPosition = new Vector2(55 * j, -55 * i);
-                text.name = (i * textArray.GetLength(0) + j + 1).ToString();
+                text.rectTransform.localPosition = new Vector2(55 * j, -45 * i);
+                text.name = (i * 5 + j + 1).ToString();
                 textArray[i, j] = text;
             }
         }
@@ -36,13 +43,13 @@ public class JournalManager : MonoBehaviour
     {
         journal.GetComponent<Canvas>().enabled = true;
         playerScript.inJournal = true;
-        for (int i = 0; i < textArray.GetLength(0); i++)
+        for (int i = 0; i < 5; i++)
         {
-            for (int j = 0; j < textArray.GetLength(1); j++)
+            for (int j = 0; j < 5; j++)
             {
-                if (hasNote[i * textArray.GetLength(0) + j])
+                if (HasNote(i * 5 + j))
                 {
-                    textArray[i, j].text = "#" + (i * textArray.GetLength(0) + j + 1).ToString();
+                    textArray[i, j].text = "#" + textArray[i, j].name;
                 }
                 else
                 {
@@ -51,6 +58,7 @@ public class JournalManager : MonoBehaviour
             }
         }
         UpdateSelector();
+        UpdateRightSide();
     }
 
     public void CloseJournal()
@@ -60,7 +68,47 @@ public class JournalManager : MonoBehaviour
     }
     public void UpdateSelector()
     {
-        textArray[Mathf.RoundToInt(selectorPos.y), Mathf.RoundToInt(selectorPos.x)].color = Color.yellow;
-        textArray[Mathf.RoundToInt(prevSelect.y), Mathf.RoundToInt(prevSelect.x)].color = Color.white;
+        textArray[Mathf.RoundToInt(selectorPos.y), Mathf.RoundToInt(selectorPos.x)].text = "<mark color=#FFFFFF50 padding=15,15,15,15>" + textArray[Mathf.RoundToInt(selectorPos.y), Mathf.RoundToInt(selectorPos.x)].text;
+        textArray[Mathf.RoundToInt(prevSelectorPos.y), Mathf.RoundToInt(prevSelectorPos.x)].text = textArray[Mathf.RoundToInt(prevSelectorPos.y), Mathf.RoundToInt(prevSelectorPos.x)].text.Replace("<mark color=#FFFFFF50 padding=15,15,15,15>","");
+        textArray[Mathf.RoundToInt(selection.y), Mathf.RoundToInt(selection.x)].color = Color.yellow;
+        textArray[Mathf.RoundToInt(selection.y), Mathf.RoundToInt(selection.x)].fontStyle = FontStyles.Bold;
+        textArray[Mathf.RoundToInt(prevSelection.y), Mathf.RoundToInt(prevSelection.x)].color = Color.white;
+        textArray[Mathf.RoundToInt(prevSelection.y), Mathf.RoundToInt(prevSelection.x)].fontStyle = FontStyles.Normal;
+    }
+    public void UpdateRightSide()
+    {
+        NoteScript curNote = GetNote(Mathf.RoundToInt(selection.y) * 5 + Mathf.RoundToInt(selection.x));
+        if (curNote != null)
+        {
+            noteTitle.text = "#" + (curNote.noteId + 1) + " - " + curNote.noteTitle;
+            noteLore.text = curNote.noteLore;
+        }
+        else
+        {
+            noteTitle.text = "#?? - ?????";
+            noteLore.text = "I don't have this note yet.";
+        }
+    }
+    public bool HasNote(int noteId)
+    {
+        foreach(NoteScript n in notes)
+        {
+            if (n.noteId == noteId)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public NoteScript GetNote(int noteId)
+    {
+        foreach (NoteScript n in notes)
+        {
+            if (n.noteId == noteId)
+            {
+                return n;
+            }
+        }
+        return null;
     }
 }
